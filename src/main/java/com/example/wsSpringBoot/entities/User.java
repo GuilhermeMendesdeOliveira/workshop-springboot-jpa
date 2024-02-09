b2d375a56ff969956cf4d62008847724c056fc2d
GuilhermeMendesdeOliveira <mendesoliveiragmo@gmail.com>
2024-02-09 12:39:02 -0300

    User entity e User Resource criado

---
+++ src/main/java/com/example/wsSpringBoot/entities/User.java
@@ -0,0 +1,42 @@
+package com.example.wsSpringBoot.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
